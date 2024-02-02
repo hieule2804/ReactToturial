5b75cc8ecda82c497f6b6176417691b704dd2b3d
hieule2804 <letrunghieu28042003@gmail.com>
2024-02-03 03:50:27 +0700

    state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import MyComponent from './components/MyConponent';
+import MyComponent from '../src/component/MyComponent';
+import React from 'react';
 
-function App() {
-  return (
-    <MyComponent></MyComponent>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+//chuyển đổi function component thanh Class Component
+class App extends React.Component {
+  render() {
+    return (
+      <div >
+        HelloWorld
+        <MyComponent></MyComponent>
+      </div>
+    )
+  }
 }
 
+
+// function App() {
+//   return (
+
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 export default App;
--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -4,12 +4,20 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    //state là 1 obj : kiểm soat data của component
+    state = {
+        name: "hihi",
+        address: "Hn",
+        age: 20
+    };
+
     //quy dinh component nay se tra ra cai gi 
     render() {
         return (
             //JSX :chir đc 1 phan tu
             <div>
-                My First Component
+                My Name is :{this.state.name} and i'm from{this.state.address}<br></br>
                 {/* viet code logic,js trong {} */}
                 {Math.random()}
             </div>
