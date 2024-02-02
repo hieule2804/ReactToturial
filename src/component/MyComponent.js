210c8aea9be5cb009eb6632c7cf24f2e3ad0dc5d
hieule2804 <letrunghieu28042003@gmail.com>
2024-02-03 05:07:19 +0700

    nesting Component

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -2,58 +2,18 @@
 //funstion component
 
 import React from "react";
+import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component {
 
-    //state là 1 obj : kiểm soat data của component
-    state = {
-        name: "hihi",
-        address: "Hn",
-        age: 20
-    };
-    handlerClick(event) {
-        console.log("my name is the : ", this.state.name)
-        // console.log("random : ", Math.floor((Math.random() * 100) + 1))
-
-        //thay doi giá trị state
-        this.setState({
-            name: "hlee",
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-    }
-    handleOnMouseOver(event) {
-        console.log(event);
-    }
-
-    handleOnChange = (event) => {
-        // console.log(event, event.target.value)
-
-        this.setState({
-            name: event.target.value
-        })
-    }
-    handleOnSubmit = (event) => {
-        //ngăn reload lại trang
-        event.preventDefault();
-        console.log(this.state.name)
-    }
 
     //quy dinh component nay se tra ra cai gi 
     render() {
         return (
             //JSX :chir đc 1 phan tu
             <div>
-                My Name is :{this.state.name} and i'm{this.state.age}<br></br>
-                {/* viet code logic,js trong {} */}
-                {Math.random()}
-                {/* chuyen qua dung arrow function */}
-                {/* <button onClick={(event) => { this.handlerClick(event) }}>Click Me</button> */}
-
-                <form onSubmit={(event) => this.handleOnSubmit(event)}>
-                    <input type="text" onChange={(event) => { this.handleOnChange(event) }}></input>
-                    <button >Submit</button>
-                </form>
-
+                {/* goi component con */}
+                <UserInfor />
             </div>
         );
     }
---
+++ src/component/UserInfor.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+class UserInfor extends React.Component {
+    //state là 1 obj : kiểm soat data của component
+    state = {
+        name: "hihi",
+        address: "Hn",
+        age: 20
+    };
+    handleOnChangeInput = (event) => {
+        // console.log(event, event.target.value)
+
+        this.setState({
+            name: event.target.value
+        })
+    }
+    handleOnChangeAge = (event) => {
+        // console.log(event, event.target.value)
+
+        this.setState({
+            age: event.target.value
+        })
+    }
+    handleOnSubmit = (event) => {
+        //ngăn reload lại trang
+        event.preventDefault();
+        console.log(this.state)
+    }
+
+
+    render() {
+        return (
+            <div>
+                My Name is :{this.state.name} and i'm{this.state.age}<br></br>
+                {/* viet code logic,js trong {} */}
+                {Math.random()}
+                {/* chuyen qua dung arrow function */}
+                {/* <button onClick={(event) => { this.handlerClick(event) }}>Click Me</button> */}
+
+                <form onSubmit={(event) => this.handleOnSubmit(event)}>
+                    <label>Your name</label>
+                    <input type="text"
+                        value={this.state.name}
+                        onChange={(event) => { this.handleOnChangeInput(event) }}>
+
+                    </input>
+                    <label>Your Age</label>
+                    <input type="text"
+                        value={this.state.age}
+                        onChange={(event) => { this.handleOnChangeAge(event) }}>
+
+                    </input>
+                    <button >Submit</button>
+                </form>
+            </div>
+        )
+
+    }
+
+}
+
+export default UserInfor;
