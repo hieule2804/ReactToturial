cc2baccc2fd3bd8815ea7ab8f5f3011407f67c29
hieule2804 <letrunghieu28042003@gmail.com>
2024-02-03 04:35:41 +0700

    setState On Component

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -12,7 +12,14 @@ class MyComponent extends React.Component {
         age: 20
     };
     handlerClick(event) {
-        // console.log("my name is the : ", this.state.name)
+        console.log("my name is the : ", this.state.name)
+        // console.log("random : ", Math.floor((Math.random() * 100) + 1))
+
+        //thay doi giá trị state
+        this.setState({
+            name: "hlee",
+            age: Math.floor((Math.random() * 100) + 1)
+        })
     }
     handleOnMouseOver(event) {
         console.log(event);
@@ -22,10 +29,11 @@ class MyComponent extends React.Component {
         return (
             //JSX :chir đc 1 phan tu
             <div>
-                My Name is :{this.state.name} and i'm from{this.state.address}<br></br>
+                My Name is :{this.state.name} and i'm{this.state.age}<br></br>
                 {/* viet code logic,js trong {} */}
                 {Math.random()}
-                <button onClick={this.handlerClick}>Click Me</button>
+                {/* chuyen qua dung arrow function */}
+                <button onClick={(event) => { this.handlerClick(event) }}>Click Me</button>
                 <button onMouseOver={this.handleOnMouseOver}>Have Me</button>
             </div>
         );
