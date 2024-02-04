b7070876a394437296e862cf522d0115122a3010
hieule2804 <letrunghieu28042003@gmail.com>
2024-02-04 23:12:52 +0700

    useEffect

--- src/component/DisplayInfor.js
+++ src/component/DisplayInfor.js
@@ -1,85 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "../logo.svg"
 
 //stateless vs stateful
-
-//class component
-// class DisplayInfor extends React.Component {
-
-//     // constructor(props) {
-//     //     console.log("call constructor:1")
-//     //     super(props);
-//     //     //babel compiler
-//     //     this.state = {
-//     //         isTheShowListUser: true
-//     //     }
-
-//     // }
-//     // //dùng hàm này để gọi API
-//     // componentDidMount() {
-//     //     console.log("call me Component did mount")
-//     //     setTimeout(() => {
-//     //         document.title = "hello"
-//     //     }, 3000);
-//     // }
-
-//     // componentDidUpdate(prevProps, prevState, snapshot) {
-//     //     console.log("call me component did update", this.props, prevProps)
-//     //     if (this.props.listUser !== prevProps.listUser) {
-//     //         if (this.props.listUser.length === 5) {
-//     //             alert("you got 5 users")
-//     //         }
-//     //     }
-
-//     // }
-
-//     // handleShowHide = () => {
-//     //     this.setState({
-//     //         isTheShowListUser: !this.state.isTheShowListUser
-//     //     })
-//     // }
-//     render() {
-//         console.log("call me render")
-//         //destructuring array/obj  : chỉ lấy ra n thuộc tính từ props
-//         const { listUser } = this.props;
-//         //const listUser = this.props.listUser
-//         //props : viet tat properties
-//         //dùng để truyền dữ liệu từ component cha sang con 
-//         return (
-//             <div className="display-infor-container">
-//                 {/* <img src={logo} /> */}
-
-//                 {true &&
-//                     <div>
-//                         {listUser.map((user) => {
-
-//                             return (
-//                                 <div key={user.id} className={user.age > 18 ? "green" : "red"}>
-//                                     <div >
-//                                         my name : {user.name}
-//                                     </div>
-//                                     <div>
-//                                         my age : {user.age}
-//                                     </div>
-//                                     <button onClick={() => { this.props.handleDeleteUser(user.id) }}>Delete</button>
-//                                 </div>
-//                             )
-//                         })}
-
-
-//                     </div>
-//                 }
-//             </div>
-
-
-//         )
-
-//     }
-
-// }
-
-
 //function component k co tu khoa this
 //tu khoa this chi ton tai trong class
 
@@ -96,6 +19,20 @@ const DisplayInfor = (props) => {
     const handleShowHideListUser = () => {
         setShowHideListUser(!isTheShowHideListUser)
     }
+    //định nghĩa bao nhiêu hook cũng đc
+
+    useEffect(
+        () => {
+            if (listUser.length === 0) {
+                alert("me")
+            }
+            console.log("call me Effect")
+        }, [listUser]
+        // mỗi lần giá trị của hàm listUser thay đổi thì hàm useEffect sẽ được gọi 
+        // arrowFunction : sẽ làm gì khi có sự thay đổi(thay đổi props or state) :
+        //[] :mảng dependence : chỉ thực hiện effect 1 lần 
+    );
+
     return (
         <div className="display-infor-container">
             {/* <img src={logo} /> */}
