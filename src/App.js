fbc9bc3894c55659f62d04657bf747f6935358f8
hieule2804 <letrunghieu28042003@gmail.com>
2024-02-05 20:42:44 +0700

    react bootStrap

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import logo from './logo.svg';
 import './App.scss';
-import MyComponent from '../src/component/MyComponent';
-import React from 'react';
+import Header from './component/header/header';
 
-//chuyển đổi function component thanh Class Component
-class App extends React.Component {
-  render() {
-    return (
-      <div className='app-container'>
-        HelloWorld
-        <MyComponent></MyComponent>
-      </div>
-    )
-  }
+
+function App() {
+  return (
+
+    <div className="App-container">
+      <Header />
+
+    </div>
+  );
 }
-
-
-// function App() {
-//   return (
-
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 export default App;
