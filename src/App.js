f2a07602d2c8267157f91c544dc2900cf00a85a8
hieule2804 <letrunghieu28042003@gmail.com>
2024-02-05 21:22:56 +0700

    dieu huong trang vs links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.scss';
 import Header from './component/header/header';
-
+import { Link } from "react-router-dom";
 
 function App() {
   return (
 
     <div className="App-container">
       <Header />
+      <div>test link
+        <div>
+          <button>
+            <Link to={`/admin`}>Go To Admin Page</Link>
+          </button>
+          <button>
+            <Link to={`/user`}>Go To User Page</Link>
+          </button>
+        </div>
 
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import User from './component/User/User';
+import Admin from './component/Admin/Admin';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/user' element={<User />} />
+      <Route path='/admin' element={<Admin />} />
+
+    </Routes>
+  </BrowserRouter>
   // </React.StrictMode>
 );
 
